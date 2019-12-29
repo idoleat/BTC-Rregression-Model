51cf2f12b3beb997749f4b039ef0bbe1a5455725
簡志瑋 <dppss92132@gmail.com>
2019-12-29 21:50:03 +0800

    Plot scatter figue of each indicator versus Close BTC.

--- Scripts/Regression_Rversion.R
+++ Scripts/Regression_Rversion.R
@@ -8,20 +8,25 @@ TrainModel <- function(trainX, trainY, testX, column, SL = 0.05){
 
 }
 
-# In[0] 讀入資料:
+# In[0] Read data:
 ## need to fix the path to relative path....
 ## R is quite annoying to do so. Alt least I haven't found the answer yet.
-files = c("C:/Users/dppss/OneDrive/E3/FinalProject/BTC.xlsx",
-            "C:/Users/dppss/OneDrive/E3/FinalProject/Gold.xlsx",
-            "C:/Users/dppss/OneDrive/E3/FinalProject/LTC.xlsx",
-            "C:/Users/dppss/OneDrive/E3/FinalProject/NDX100.xlsx",
-            "C:/Users/dppss/OneDrive/E3/FinalProject/Oil.xlsx")
+files = c("D:/BTC Regression Model/Data/BTC.xlsx",
+            "D:/BTC Regression Model/Data/Gold.xlsx",
+            "D:/BTC Regression Model/Data/LTC.xlsx",
+            "D:/BTC Regression Model/Data/NDX100.xlsx",
+            "D:/BTC Regression Model/Data/Oil.xlsx")
 df <- read.xlsx(file = files[1], sheetName = 1)
 # http://r3dmaotech.blogspot.com/2016/09/r-data-frame-merge-join.html
 for(f in files[2:length(files)]){
     df = merge(df, read.xlsx(f, 1), by = "Date")
 }
 
-# In[1] 資料分析:
+# In[1] Data analyze:
 
-# 繪出各Feature與Close BTC之間的散佈圖。
+# Plot scatter figue of each indicator versus Close BTC.
+ColumnCount = ncol(df)
+par(mfrow = c(ColumnCount/3,3), mai =  c(0.3, 0.3, 0.3, 0.3), cex = 0.6)
+for(i in 3:ColumnCount){
+    plot(x = df[,2], y = df[,i], main = names(df)[i])
+}
